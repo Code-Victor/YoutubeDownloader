fca5c1fc11c4669829c5bdb8072ee4ef9f01166d
Hamzat Victor Oluwabori <oluwaborihamzat@gmail.com>
2022-03-18 19:59:22 +0100

    added packages requirement

--- script.py
+++ script.py
@@ -6,8 +6,8 @@ from docx import Document
 import os
 
 
-class getyoutubevideo:
-    def __init__(self,url,destinaiton=r"C:\users\oluwa\tutorials",audio=False,resolution='480p',Transcript=True,language=['en']):
+class GetYoutubeVideo:
+    def __init__(self,url,destinaiton=r"C:\users\oluwa\tutorials",audio=False,resolution='720p',Transcript=True,language=['en']):
         self.url=url
         self.destination=destinaiton
         self.transcript=Transcript
@@ -15,7 +15,7 @@ class getyoutubevideo:
         self.audio=audio
         self.transcript_language=language
         self.videoId=extract.video_id(self.url)
-        self.yt=YouTube(self.url)
+        self.yt=YouTube(self.url,on_progress_callback=on_progress)
         self.name=self.yt.title
         for word, initial in {'?':'.','<':'.','>':'.','|':'.','*':'.'}.items():
             self.name = self.name.replace(word.lower(), initial)
@@ -24,45 +24,43 @@ class getyoutubevideo:
 
 
 
-    def getTranscript(self):
+    def get_transcript(self):
+        print("downloading {}.vtt".format(self.name))
         transcript=ytTrans.get_transcript(self.videoId,languages=self.transcript_language)
         webvtt_formatter=WebVTTFormatter()
         webvtt_formatted=webvtt_formatter.format_transcript(transcript=transcript)
-        with open('{}.vtt'.format(self.name), 'w', encoding='utf-8') as webvtt_file:
+        with open('./test/{}.vtt'.format(self.name), 'w', encoding='utf-8') as webvtt_file:
             webvtt_file.write(webvtt_formatted)
+        print("downloaded {}.vtt".format(self.name))
 
-    def _getDescription(self):
+    def _get_description(self):
+
+        print("downloading {}.docx".format(self.name))
         document=Document()
         description=self.yt.description
         document.add_heading(self.name,0)
         document.add_paragraph(description)
-        document.save('{}.docx'.format(self.name))
+        document.save('./test/{}.docx'.format(self.name))
         stream=self.yt.streams.filter(res=self.resolution)
+        print("downloaded {}.docx".format(self.name))
          
         
 
-    def getVideo(self):
-        if self.audio==True:
-            if self.audio_quality:
-                try:
-                    for stream in self.yt.streams.filter(only_audio=True):
-                        if stream.abr == self.audio_quality:
-                            stream.download()
-                except:
-                    self.yt.streams.filter(only_audio=True).first().download()
-            else:
-                stream=self.yt.streams.filter(only_audio=True).order_by("abr")[-1]
-                stream.download()
-        elif self.audio==False:
-            stream=self.yt.streams.filter(res=self.resolution)  
-            stream.download()
-    def alltogether(self):
-        self.getTranscript()
-        self._getDescription()
-        self.getVideo()
+    def get_video(self):
+        stream=self.yt.streams.filter(res=self.resolution,progressive=True)  
+        stream.first().download('./test/')
+        print("downloaded {}".format(self.name))
+
+
+    def all_together(self):
+        self.get_transcript()
+        self._get_description()
+        self.get_video()
     
 
 # youtube=getyoutubevideo(url="https://www.youtube.com/watch?v=VEQaH4LruUo")
 # youtube.alltogether()
-youtube=getyoutubevideo(url="https://youtu.be/_YPScrckx28")
-# youtube.alltogether()
+
+# print(YouTube("https://youtu.be/_YPScrckx28").streams.filter(progressive=True).get_highest_resolution().resolution)
+youtube=GetYoutubeVideo(url="https://www.youtube.com/watch?v=zrs7u6bdbUw")
+youtube.all_together()
